ea56f1bc1074d44960162c7c8c0b1285bd1812bf
gyuri <fnffkdll@naver.com>
2020-08-05 07:27:22 +0900

    update - main - responsible

--- js/index.js
+++ js/index.js
@@ -3,91 +3,157 @@ $(document).ready(function(){
     //Plug-in AOS
     AOS.init();
     
+    //scrollwidth
+    var screenWidth = $(window).width();
+    
+    
+    
     //main >> main_vis -- text scroll masic ---------------------
-    
-    var controller = new ScrollMagic.Controller();
-    
-    //vis_tit first txt
-    var tween1 = TweenMax.to('.vis_tit .link.t_f', 0.5, {
-        top: -100,
-        opacity: 0,
-    });
-    
-    var scene = new ScrollMagic.Scene({
-        triggerElement: ".main_vis .con",
-        duration: "20%",
-        offset : +250,
-    })
-    .setTween(tween1)
-    .addTo(controller)
-    .addIndicators({
-        name: "1"
-    })
-    
-    //vis_tit second txt
-    var tween2 = TweenMax.to('.vis_tit .link.t_s', 0.5, {
-        top: -100,
-        opacity: 0,
-    });
-    
-    var scene = new ScrollMagic.Scene({
-        triggerElement: ".main_vis .con",
-        duration: "20%",
-        offset : +320,
-    })
-    .setTween(tween2)
-    .addTo(controller)
-    .addIndicators({
-        name: "2"
-    })
-    
-    //vis_tit third txt
-    var tween3 = TweenMax.to('.vis_tit .link.t_t', 0.5, {
-        top: -100,
-        opacity: 0,
-    });
-    
-    var scene = new ScrollMagic.Scene({
-        triggerElement: ".main_vis .con",
-        duration: "20%",
-        offset : +400,
-    })
-    .setTween(tween3)
-    .setTween(tween4)
-    .addTo(controller)
-    .addIndicators({
-        name: "3"
-    })
-    
-    
-    //vis_img
-    var tween4 = TweenMax.to('.vis_img', 0.5, {
-        opacity: 0,
-    });
-    
-    var scene = new ScrollMagic.Scene({
-        triggerElement: ".main_vis .con",
-        duration: "20%",
-        offset : +400,
-    })
-    .setTween(tween4)
-    .addTo(controller)
-    .addIndicators({
-        name: "4"
-    })
-    
-    
+    if( screenWidth > 768){
+        var controller = new ScrollMagic.Controller();
+
+        //vis_tit first txt
+        var tween1 = TweenMax.to('.vis_tit .link.t_f', 0.5, {
+            top: -100,
+            opacity: 0,
+        });
+
+        var scene = new ScrollMagic.Scene({
+            triggerElement: ".main_vis .con",
+            duration: "20%",
+            offset : +250,
+        })
+        .setTween(tween1)
+        .addTo(controller)
+//        .addIndicators({
+//            name: "vis1"
+//        })
+
+        //vis_tit second txt
+        var tween2 = TweenMax.to('.vis_tit .link.t_s', 0.5, {
+            top: -100,
+            opacity: 0,
+        });
+
+        var scene = new ScrollMagic.Scene({
+            triggerElement: ".main_vis .con",
+            duration: "20%",
+            offset : +320,
+        })
+        .setTween(tween2)
+        .addTo(controller)
+//        .addIndicators({
+//            name: "vis2"
+//        })
+
+        //vis_tit third txt
+        var tween3 = TweenMax.to('.vis_tit .link.t_t', 0.5, {
+            top: -100,
+            opacity: 0,
+        });
+
+        var scene = new ScrollMagic.Scene({
+            triggerElement: ".main_vis .con",
+            duration: "20%",
+            offset : +400,
+        })
+        .setTween(tween3)
+        .setTween(tween4)
+        .addTo(controller)
+//        .addIndicators({
+//            name: "vis3"
+//        })
 
 
-    /* Intro - Tilt */
-//    setInterval(function () {
-//      $('.main_vis .con').tilt({
-//        reset: false,
-//        speed: 400,
-//        maxTilt: 5,
-//      });
-//    }, 6400);
+        //vis_img
+        var tween4 = TweenMax.to('.vis_img', 0.5, {
+            opacity: 0,
+        });
 
+        var scene = new ScrollMagic.Scene({
+            triggerElement: ".main_vis .con",
+            duration: "20%",
+            offset : +400,
+        })
+        .setTween(tween4)
+        .addTo(controller)
+//        .addIndicators({
+//            name: "vis4"
+//        })
+    }
+    if( screenWidth <= 768){
+                var controller = new ScrollMagic.Controller();
+
+        //vis_tit first txt
+        var tween1 = TweenMax.to('.vis_tit .link.t_f', 0.5, {
+            top: -100,
+            opacity: 0,
+        });
+
+        var scene = new ScrollMagic.Scene({
+            triggerElement: ".main_vis .con",
+            duration: "20%",
+            offset : +300,
+        })
+        .setTween(tween1)
+        .addTo(controller)
+//        .addIndicators({
+//            name: "vis1"
+//        })
+
+        //vis_tit second txt
+        var tween2 = TweenMax.to('.vis_tit .link.t_s', 0.5, {
+            top: -100,
+            opacity: 0,
+        });
+
+        var scene = new ScrollMagic.Scene({
+            triggerElement: ".main_vis .con",
+            duration: "20%",
+            offset : +370,
+        })
+        .setTween(tween2)
+        .addTo(controller)
+//        .addIndicators({
+//            name: "vis2"
+//        })
+
+        //vis_tit third txt
+        var tween3 = TweenMax.to('.vis_tit .link.t_t', 0.5, {
+            top: -100,
+            opacity: 0,
+        });
+
+        var scene = new ScrollMagic.Scene({
+            triggerElement: ".main_vis .con",
+            duration: "20%",
+            offset : +450,
+        })
+        .setTween(tween3)
+        .setTween(tween4)
+        .addTo(controller)
+//        .addIndicators({
+//            name: "vis3"
+//        })
+
+
+        //vis_img
+        var tween4 = TweenMax.to('.vis_img', 0.5, {
+            opacity: 0,
+        });
+
+        var scene = new ScrollMagic.Scene({
+            triggerElement: ".main_vis .con",
+            duration: "20%",
+            offset : +450,
+        })
+        .setTween(tween4)
+        .addTo(controller)
+//        .addIndicators({
+//            name: "vis4"
+//        })
+    }
+    
     
     
     // main >> main_about >> greet -- text change event  ---------------------
@@ -146,110 +212,231 @@ $(document).ready(function(){
     
     
     //main >> main_about >> keyword -- text scroll masic  ---------------------                             settween 이거 하나로 정리 & 위에 vis도
-    var controller = new ScrollMagic.Controller();
-	
-	var key1 = TweenMax.to('.key_1', 0.5, {
-		rotation: -60,
-		x: -140,
-		y: -110
-	});
     
-	
-	var scene = new ScrollMagic.Scene({
-		triggerElement: ".keyword",
-		duration: "120%",
-        offset : +100,
-	})
-	.setTween(key1)
-	.addTo(controller)
-	.addIndicators({
-		name: "1"
-	}) 
+    if( screenWidth > 768){
+        var controller = new ScrollMagic.Controller();
+
+        var key1 = TweenMax.to('.key_1', 0.5, {
+            rotation: -60,
+            x: -140,
+            y: -110
+        });
+
+
+        var scene = new ScrollMagic.Scene({
+            triggerElement: ".keyword",
+            duration: "120%",
+            offset : +100,
+        })
+        .setTween(key1)
+        .addTo(controller)
+//        .addIndicators({
+//            name: "1"
+//        }) 
+
+        var key2 = TweenMax.to('.key_2', 0.5, {
+            rotation: 40,
+            x: 100,
+            y: -140
+        });
+
+        var scene = new ScrollMagic.Scene({
+            triggerElement: ".keyword",
+            duration: "120%",
+            offset : +100,
+        })
+        .setTween(key2)
+        .addTo(controller)
+//        .addIndicators({
+//            name: "2"
+//        }) 
+
+        var key3 = TweenMax.to('.key_3', 0.5, {
+            rotation: 30,
+            x: -100,
+            y: 100
+        });
+
+        var scene = new ScrollMagic.Scene({
+            triggerElement: ".keyword",
+            duration: "120%",
+            offset : +100,
+        })
+        .setTween(key3)
+        .addTo(controller)
+//        .addIndicators({
+//            name: "3"
+//        }) 
+
+         var key4 = TweenMax.to('.key_4', 0.5, {
+            rotation: 40,
+            x: 80,
+            y: 110
+        });
+
+        var scene = new ScrollMagic.Scene({
+            triggerElement: ".keyword",
+            duration: "120%",
+            offset : +100,
+        })
+        .setTween(key4)
+        .addTo(controller)
+//        .addIndicators({
+//            name: "4"
+//        }) 
+    }
     
-    var key2 = TweenMax.to('.key_2', 0.5, {
-		rotation: 40,
-		x: 100,
-		y: -140
-	});
     
-    var scene = new ScrollMagic.Scene({
-		triggerElement: ".keyword",
-		duration: "120%",
-        offset : +100,
-	})
-	.setTween(key2)
-	.addTo(controller)
-	.addIndicators({
-		name: "2"
-	}) 
+//    if( screenWidth > 480 ){
+//        var controller = new ScrollMagic.Controller();
+//
+//        var key1 = TweenMax.to('.key_1', 0.5, {
+//            rotation: -60,
+//            x: -80,
+//            y: -50
+//        });
+//
+//
+//        var scene = new ScrollMagic.Scene({
+//            triggerElement: ".keyword",
+//            duration: "100%",
+//            offset : 0,
+//        })
+//        .setTween(key1)
+//        .addTo(controller)
+////        .addIndicators({
+////            name: "1"
+////        }) 
+//
+//        var key2 = TweenMax.to('.key_2', 0.5, {
+//            rotation: 40,
+//            x: 40,
+//            y: -80
+//        });
+//
+//        var scene = new ScrollMagic.Scene({
+//            triggerElement: ".keyword",
+//            duration: "100%",
+//            offset : 0,
+//        })
+//        .setTween(key2)
+//        .addTo(controller)
+////        .addIndicators({
+////            name: "2"
+////        }) 
+//
+//        var key3 = TweenMax.to('.key_3', 0.5, {
+//            rotation: 30,
+//            x: -40,
+//            y: 40
+//        });
+//
+//        var scene = new ScrollMagic.Scene({
+//            triggerElement: ".keyword",
+//            duration: "100%",
+//            offset : 0,
+//        })
+//        .setTween(key3)
+//        .addTo(controller)
+////        .addIndicators({
+////            name: "3"
+////        }) 
+//
+//         var key4 = TweenMax.to('.key_4', 0.5, {
+//            rotation: 40,
+//            x: 20,
+//            y: 50
+//        });
+//
+//        var scene = new ScrollMagic.Scene({
+//            triggerElement: ".keyword",
+//            duration: "100%",
+//            offset : 0,
+//        })
+//        .setTween(key4)
+//        .addTo(controller)
+////        .addIndicators({
+////            name: "4"
+////        }) 
+//    }
     
-    var key3 = TweenMax.to('.key_3', 0.5, {
-		rotation: 30,
-		x: -100,
-		y: 100
-	});
+    if( screenWidth <= 480 ){
+        var controller = new ScrollMagic.Controller();
+
+        var key1 = TweenMax.to('.key_1', 0.5, {
+            rotation: -60,
+            x: -60,
+            y: -80
+        });
+
+
+        var scene = new ScrollMagic.Scene({
+            triggerElement: ".keyword",
+            duration: "100%",
+            offset : 0,
+        })
+        .setTween(key1)
+        .addTo(controller)
+//        .addIndicators({
+//            name: "1"
+//        }) 
+
+        var key2 = TweenMax.to('.key_2', 0.5, {
+            rotation: 40,
+            x: 0,
+            y: -80
+        });
+
+        var scene = new ScrollMagic.Scene({
+            triggerElement: ".keyword",
+            duration: "100%",
+            offset : 0,
+        })
+        .setTween(key2)
+        .addTo(controller)
+//        .addIndicators({
+//            name: "2"
+//        }) 
+
+        var key3 = TweenMax.to('.key_3', 0.5, {
+            rotation: 30,
+            x: -20,
+            y: 20
+        });
+
+        var scene = new ScrollMagic.Scene({
+            triggerElement: ".keyword",
+            duration: "100%",
+            offset : 0,
+        })
+        .setTween(key3)
+        .addTo(controller)
+//        .addIndicators({
+//            name: "3"
+//        }) 
+
+         var key4 = TweenMax.to('.key_4', 0.5, {
+            rotation: 40,
+            x: 10,
+            y: 20
+        });
+
+        var scene = new ScrollMagic.Scene({
+            triggerElement: ".keyword",
+            duration: "100%",
+            offset : 0,
+        })
+        .setTween(key4)
+        .addTo(controller)
+//        .addIndicators({
+//            name: "4"
+//        }) 
+    }
     
-    var scene = new ScrollMagic.Scene({
-		triggerElement: ".keyword",
-		duration: "120%",
-        offset : +100,
-	})
-	.setTween(key3)
-	.addTo(controller)
-	.addIndicators({
-		name: "3"
-	}) 
     
-     var key4 = TweenMax.to('.key_4', 0.5, {
-		rotation: 40,
-		x: 80,
-		y: 110
-	});
-    
-    var scene = new ScrollMagic.Scene({
-		triggerElement: ".keyword",
-		duration: "120%",
-        offset : +100,
-	})
-	.setTween(key4)
-	.addTo(controller)
-	.addIndicators({
-		name: "4"
-	}) 
-    
-    $('.new_slide_type a').click(function(){
-        $('.new_slide_type').slideUp();
-        
-        //버튼 글씨
-        $('.new_slide_label button').text($(this).text());
-        
-        idx = $(this).index() + 1;
-        //슬라이드
-        $('.new_slide1, .new_slide2, .new_slide3').removeClass('on');
-        $('.new_slide' + idx).addClass('on');
-        
-        //네비게이션
-        $('.slide1_nav, .slide2_nav, .slide3_nav').removeClass('on');
-        $('.slide' + idx + '_nav').addClass('on');
-        
-        //배너
-        $('.new_slide_banner>img').attr('src','img/banner/bn_new_slide' + idx + '.jpg')
-    });
     
     
     //main >> main_about >> keyword -- hover event  ---------------------                                         반복문 정리하기
-    
-//    var key = $('.key').index();
-//    
-//    for( var i=0; i<key.length; i++){
-//        var idx = [i] + 1;
-//        
-//        $('.key_t_'+idx).hover(function(){
-//            $('.key_'+idx).css({backgroundColor:'#d2687b'});
-//        },function(){
-//            $('.key_'+idx).css({backgroundColor:'#708658'});
-//        })
-//    }
     
     $('.key_t_1').hover(function(){
         $('.key_1').css({backgroundColor:'#d2687b'});
@@ -275,83 +462,119 @@ $(document).ready(function(){
     
     //main >> main_proj -- scroll pin -----------------
     
-    var controller = new ScrollMagic.Controller({
-        globalSceneOptions: {
-            triggerHook: 'onLeave',
-            duration: "50%"
-        }
-	});
- 	var slides = document.querySelectorAll(".project");
+    
+    if( screenWidth > 768){
+        var controller = new ScrollMagic.Controller({
+            globalSceneOptions: {
+                triggerHook: 'onLeave',
+                duration: "50%"
+            }
+        });
+        var slides = document.querySelectorAll(".project");
 
-	for (var i=0; i<slides.length; i++) {
-		var scene = new ScrollMagic.Scene({
-			triggerElement: slides[i]
-		})
-		.setPin(slides[i], {pushFollowers: false})
-		.addIndicators() 
-		.addTo(controller);
-	}
+        for (var i=0; i<slides.length; i++) {
+            var scene = new ScrollMagic.Scene({
+                triggerElement: slides[i]
+            })
+            .setPin(slides[i], {pushFollowers: false})
+//            .addIndicators() 
+            .addTo(controller);
+        }
+    }
+    
+    
+    
     
     
     //main >> main_proj -- scroll bg color ----------------
-    var bg2 = TweenMax.to('.bg', 0.5, {
-		backgroundColor: '#0d4432',
-	});
+    if( screenWidth > 768){
+        var bg2 = TweenMax.to('.bg', 0.5, {
+            backgroundColor: '#0d4432',
+        });
+
+        var scene = new ScrollMagic.Scene({
+            triggerElement: ".proj_2",
+            duration: "100%",
+            offset : -350,
+        })
+        .setTween(bg2)
+        .addTo(controller)
+//        .addIndicators({
+//            name: "bg2"
+//        }) 
+
+        var bg3 = TweenMax.to('.bg', 0.5, {
+            backgroundColor: '#005982',
+        });
+
+        var scene = new ScrollMagic.Scene({
+            triggerElement: ".proj_3",
+            duration: "100%",
+            offset : -350,
+        })
+        .setTween(bg3)
+        .addTo(controller)
+//        .addIndicators({
+//            name: "bg3"
+//        }) 
+    }
     
-    var scene = new ScrollMagic.Scene({
-		triggerElement: ".proj_2",
-		duration: "100%",
-        offset : -350,
-	})
-	.setTween(bg2)
-	.addTo(controller)
-	.addIndicators({
-		name: "bg2"
-	}) 
     
-    var bg3 = TweenMax.to('.bg', 0.5, {
-		backgroundColor: '#005982',
-	});
+    //main >> main_contact -- scroll h4 scale ----------------
+    if( screenWidth > 768){
+        var h4 = TweenMax.to('.main_cont h4', 1, {
+            scale: 80,
+            x: -750,
+        });
+
+        var scene = new ScrollMagic.Scene({
+            triggerElement: ".main_cont",
+            duration: "100%",
+            offset: 0,
+        })
+        .setTween(h4)
+        .setPin(h4, {pushFollowers: false})
+        .addTo(controller)
+//        .addIndicators({
+//            name: "h4"
+//        }) 
+    }
+    if( screenWidth <= 768){
+        var h4 = TweenMax.to('.main_cont h4', 1, {
+            scale: 110,
+            x: -400,
+        });
+
+        var scene = new ScrollMagic.Scene({
+            triggerElement: ".main_cont",
+            duration: "100%",
+            offset: 150,
+        })
+        .setTween(h4)
+        .setPin(h4, {pushFollowers: false})
+        .addTo(controller)
+//        .addIndicators({
+//            name: "h4"
+//        }) 
+    }
     
-    var scene = new ScrollMagic.Scene({
-		triggerElement: ".proj_3",
-		duration: "100%",
-        offset : -350,
-	})
-	.setTween(bg3)
-	.addTo(controller)
-	.addIndicators({
-		name: "bg3"
-	}) 
-    
-    
-    
-    
-    var h4 = TweenMax.to('.main_cont h4', 1, {
-		scale: 80,
-        x: -700,
-	});
-    
-    var scene = new ScrollMagic.Scene({
-		triggerElement: ".main_cont",
-		duration: "100%",
-        offset: 0,
-	})
-	.setTween(h4)
-    .setPin(h4, {pushFollowers: false})
-	.addTo(controller)
-	.addIndicators({
-		name: "h4"
-	}) 
     
     
     /* main >> contact -- Tilt */
-    setInterval(function () {
-      $('.main_cont .img_wrap').tilt({
-        reset: false,
-        speed: 400,
-        maxTilt: 20,
-      });
-    }, 6400);
+    if( screenWidth > 768){
+        setInterval(function () {
+          $('.main_cont .img_wrap').tilt({
+            reset: false,
+            speed: 400,
+            maxTilt: 20,
+          });
+        }, 6400);
+    }
+    
+    if( screenWidth <= 768){
+        $('.link--kukuri').removeClass('link--kukuri');
+    }
+    
+    
   
 })
